a0fb2ffbf49d292a17ba1762b484323414f50f9d
mgrajez <monikagrajez@gmail.com>
2023-11-14 16:46:42 +0100

    Fix removed tailed /api

--- src/services/service.js
+++ src/services/service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL =
-  `${import.meta.env.VITE_BACKEND_URL}/api` || "https://stokk.netlify.app";
+  `${import.meta.env.VITE_BACKEND_URL}` || "https://stokk.netlify.app";
 
 const api = axios.create({
   baseURL: baseURL,
