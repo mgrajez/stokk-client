23bb0e7ffede2ed083c01a6bd9203fcbf7847a2d
mgrajez <monikagrajez@gmail.com>
2023-11-30 13:54:27 +0100

    Updated loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,13 +3,9 @@ import "./Loader.css";
 
 function Loader() {
   return (
-    // <div className="loader-container">
-    //   {/* Your loader content (e.g., spinner, animation, etc.) */}
-    //   Loading...
-    // </div>
-    <div id="loading-container">
-      <p id="loading-text">Loading</p>
-      <div id="progress-bar"></div>
+    <div className="loading-container">
+      <p className="loading-text">Loading</p>
+      <div className="progress-bar"></div>
     </div>
   );
 }
